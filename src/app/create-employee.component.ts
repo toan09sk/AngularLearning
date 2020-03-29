5abda70157b4ad01f9c5746dbec41967dc244d1e
Ross <phambatoansk@gmail.com>
2020-03-29 17:44:20 +0700

    video 6: Bootstrap checkbox in angular

--- src/app/create-employee.component.ts
+++ src/app/create-employee.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
   gender = 'male';
+  isActive = true;
+  
   constructor() { }
 
   ngOnInit() {
