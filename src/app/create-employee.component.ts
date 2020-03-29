c2a4acf490a18889e5ead16315c1874f6da79247
Ross <phambatoansk@gmail.com>
2020-03-29 20:13:09 +0700

    Edit UI app.component and remove selector of navigation component

--- src/app/create-employee.component.ts
+++ src/app/create-employee.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
   styleUrls: ['./create-employee.component.scss']
 })
