0a0ad0b4968b5c02e786fb463fb48e5a3cca956f
Ross <phambatoansk@gmail.com>
2020-04-04 01:04:03 +0700

    video 38: Subscribe to angular route parameter changes

--- src/app/employees/employee-details.component.ts
+++ src/app/employees/employee-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from './employee.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -11,13 +11,26 @@ import { Employee } from '../models/employee.model';
 export class EmployeeDetailsComponent implements OnInit {
 
   employee: Employee;
+  private _id;
   constructor(private _route: ActivatedRoute,
-    private _employeeService: EmployeeService) { }
+    private _employeeService: EmployeeService,
+    private _router: Router) { }
 
   ngOnInit() {
-    // Convert string to int "+"
-    const id = +this._route.snapshot.paramMap.get('id');
-    this.employee = this._employeeService.getEmployee(id);
+    this._route.params.subscribe(params => {
+      this._id = +params['id'];
+      this.employee = this._employeeService.getEmployee(this._id);
+    });
+  }
+
+  getNextEmployee() {
+    if (this._id < 3) {
+      this._id = this._id + 1;
+    } else {
+      this._id = 1;
+    }
+
+    this._router.navigate(['/employees', this._id]);
   }
 
 }
